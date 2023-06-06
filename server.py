8f7ea99021bb59373a105ef4e99aee8407ebee73
sevase <kotaseva8@gmail.com>
2023-06-06 12:03:11 +0300

    подключение клиента

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import subprocess
 import os
 
-HOST, PORT = 'adminLOG', 8080
+HOST, PORT = '', 12345
 
 # создание сокета, привязка
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # сокет;  IPv4, TPC
@@ -13,7 +13,8 @@ server_socket.listen(5)
 print('Сервер был запущен... Ожидает подключение клиента...')
 
 # подключен клиент
-
+sock_client, sock_adress = server_socket.accept()
+print(f'Клиент подключен: [*{sock_adress}*]  [*{sock_client}*]')
 
 
 
