2e428cae501ed0f6ceea098a116653e9dcb8ee90
sevase <kotaseva8@gmail.com>
2023-06-06 11:54:05 +0300

    сделал сокет и его приявзку

--- server.py
+++ server.py
@@ -1,3 +1,17 @@
 import socket
 import subprocess
 import os
+
+HOST, PORT = 'adminLOG', 8080
+
+# создание сокета, привязка
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # сокет;  IPv4, TPC
+server_socket.bind((HOST, PORT))
+
+
+
+
+
+
+
+
