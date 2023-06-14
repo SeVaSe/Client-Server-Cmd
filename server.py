c5d13f981594322f0f76ddb73285b9a91861e4e6
sevase <kotaseva8@gmail.com>
2023-06-14 23:45:10 +0300

    сделал функции send_cmd и start_server + поток постоянных подключений

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
 import subprocess
 import os
+import threading
+
 
 # функция по окраске текста
 def colorize(text, color_code):
@@ -14,63 +16,72 @@ def colorize(text, color_code):
         return f'{text}'
 
 
-HOST, PORT = 'localhost', 12345
+# функция запуска сервера
+def start_server():
+    HOST, PORT = '192.168.0.103', 12345
 
-# создание сокета, привязка
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # сокет;  IPv4, TPC
-server_socket.bind((HOST, PORT))
+    # создание сокета, привязка
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # сокет;  IPv4, TPC
+    server_socket.bind((HOST, PORT))
 
-# прослушивание клиентов (ожидание их подключения)
-server_socket.listen(5)
-print(colorize('Сервер был запущен... Ожидает подключение клиента...', 'green'))
-
-# подключен клиент
-sock_client, sock_adress = server_socket.accept()
-print(colorize(f'Клиент подключен: [*{sock_adress}*]  [*{sock_client}*]\n', 'green'))
+    # прослушивание клиентов (ожидание их подключения)
+    server_socket.listen(5)
+    print(colorize('Сервер был запущен... Ожидает подключение клиента...', 'green'))
 
 
-# обработка комманд cmd
-while True:
-    cmd = sock_client.recv(1024).decode('utf-8')
+    while True:
+        # подключен клиент
+        sock_client, sock_adress = server_socket.accept()
+        print(colorize(f'Клиент подключен: [*{sock_adress}*]  [*{sock_client}*]\n', 'green'))
 
-    if cmd is None:
-        break
-
-    print(f'{colorize("Команда от клиента:", "yellow")} [- {cmd} -]')
+        client_thread = threading.Thread(target=send_cmd, args=(sock_client, sock_adress))
+        client_thread.start()
 
 
-    try:
-        #  Создается новый процесс, в котором выполняется команда cmd. shell=True указывает на то, что команда будет выполнена через командную оболочку
-        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                   stdin=subprocess.PIPE)
-        output, error = process.communicate()
+# функция работы cmd запросов
+def send_cmd(sock_client, sock_adress):
+    # обработка комманд cmd
+    while True:
+        cmd = sock_client.recv(1024).decode('utf-8')
 
-        # проверка винда или линукс это, для установки соотвествующей кодировки, чтобы русский текст выводился и на линукс, и на винде
-        if output:
-            if os.name == 'nt':
-                answer = output.decode('cp866')
+        if cmd is None:
+            break
+
+        print(f'{colorize("Команда от клиента:", "yellow")} [- {cmd} -]')
+
+
+        try:
+            #  Создается новый процесс, в котором выполняется команда cmd. shell=True указывает на то, что команда будет выполнена через командную оболочку
+            process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                       stdin=subprocess.PIPE)
+            output, error = process.communicate()
+
+            # проверка винда или линукс это, для установки соотвествующей кодировки, чтобы русский текст выводился и на линукс, и на винде
+            if output:
+                if os.name == 'nt':
+                    answer = output.decode('cp866')
+                else:
+                    answer = output.decode('utf-8')
             else:
-                answer = output.decode('utf-8')
-        else:
-            if os.name == 'nt':
-                answer = error.decode('cp866')
-            else:
-                answer = error.decode('utf-8')
+                if os.name == 'nt':
+                    answer = error.decode('cp866')
+                else:
+                    answer = error.decode('utf-8')
 
-        # # выполнение команд
-        # output = subprocess.check_output(cmd, shell=True)
-        # answer = output.decode('cp866')
-    except Exception as e:
-        answer = str(e)
+            # # выполнение команд
+            # output = subprocess.check_output(cmd, shell=True)
+            # answer = output.decode('cp866')
+        except Exception as e:
+            answer = str(e)
 
 
-    # отправка ответа
-    sock_client.sendall(answer.encode('utf-8'))
+        # отправка ответа
+        sock_client.sendall(answer.encode('utf-8'))
 
 
 
-server_socket.close()
-sock_client.close()
 
 
+
+start_server()
 
